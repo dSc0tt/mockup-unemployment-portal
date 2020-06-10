3163e6b35afc739263c864d313be6cb9520246cf
indigo77072 <indigo.emanuel@gmail.com>
2020-06-10 17:10:37 -0400

    I added the admin view specific claim front end page as well as printing functionality

---
+++ adminViewSpecificClaimPrint.php
@@ -0,0 +1,61 @@
+<?php
+// PHP Select statement including all claim information where claim or user id = the id of the claim that was clicked on. 
+
+?>
+<html>
+    <head>
+    <style>
+      #main {
+            height:500px;
+        }
+    </style>
+        <title>AdminLogin</title>
+        <!-- CSS only -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+<!-- JS, Popper.js, and jQuery -->
+    </head>
+    <body onLoad="window.print()">
+    
+        <p style= "text-align:center;">*PUT Name of Claim Person Here* <!-- <php echo $fullname>--></p>
+        <div class="container">
+            <div class="row" id="main">
+                <div class="col-md-3"></div>
+                <div class="col-md-6">
+                <table Class="table">
+                <tr>
+                    <td>
+                    <!--Echo out data in each table data. Add more if needed -->
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                    <!--Echo out data in each table data. Add more if needed -->
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                    <!--Echo out data in each table data. Add more if needed -->
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                    <!--Echo out data in each table data. Add more if needed -->
+                    </td>
+                </tr>
+                <tr>
+
+                </tr>
+                
+                </table> 
+                </div>
+                <div class="col-md-3"></div>
+            </div>
+            <div class="jumbotron footer" style="margin-bottom:0; background-color: white">
+            <p style="text-align:center;"> ©BdpaUnemploymentInc.</p>
+            </div>
+       </div>
+    </body>
+</html>
